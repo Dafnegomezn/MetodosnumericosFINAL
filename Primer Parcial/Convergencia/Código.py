ea4ab0ada9503086d30b61ffe5ad0b4c94fa739d
Dafnegomezn <42162178+Dafnegomezn@users.noreply.github.com>
2018-11-20 12:18:23 -0600

    Create Código.py

---
+++ Primer Parcial/Convergencia/Código.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def xnew(x):
+    return (2*x**2 +3)/5
+x0 = 0
+x1 = 0
+iteraciones = 0
+x0array = np.zeros(100)
+x1array = np.zeros(100)
+xexe = np.zeros(100)
+for i in range(100):
+    x1 = xnew(x0)
+    xexe[i]=i
+    x0array[i]= x0
+    x1array[i]=x1
+    if abs(x1-x0) < 0.00000001:
+        break
+    x0 = x1
+    iteraciones += 1
+
+plt.plot(xexe,x0array, x1array)
+plt.grid()
